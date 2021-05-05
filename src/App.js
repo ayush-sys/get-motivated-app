d5e11b77643a161b1be633967a9b6e6411199f8a
ayush-sys <ayushmp7@gmail.com>
2021-05-05 12:27:31 +0530

    Added content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
+import axios from 'axios';
 import React from 'react';
-import './CSS/App.css'
+import './App.css'
 
-const App = () => {
-  return ( 
-    <div className='head'>
-      <h1>Hello !</h1>
-      <h3>My first React Application !</h3>
-    </div>
-   );
+
+class App extends React.Component{
+
+  state = {quote:''};
+
+  componentDidMount(){
+      this.fetchQuote();
+  }
+
+  fetchQuote = () => {
+    axios.get('https://api.adviceslip.com/advice').then((response) => {
+      const {advice} = response.data.slip;
+      this.setState({quote:advice});
+      
+    })
+    .catch((err) =>{
+      console.log(err);
+    });
+  }
+
+
+  render(){
+    const {quote} = this.state;
+    return ( 
+      <div className='app'>
+        <div className='content-card'> 
+          <h1 className='heading'>Hello Ayush!</h1>
+            <h3>{quote}</h3>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default App;
